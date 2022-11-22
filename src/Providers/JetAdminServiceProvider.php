26388e3f03cbc428075d681ff30a727615be6911
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2022-11-22 04:55:48 +0330

    Update All To Packages.

--- src/Providers/JetAdminServiceProvider.php
+++ src/Providers/JetAdminServiceProvider.php
@@ -4,5 +4,17 @@
 use Illuminate\Support\ServiceProvider;
 
 class JetAdminServiceProvider extends ServiceProvider {
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'jetadmin');
+
+        $this->publishes(
+            [__DIR__ . '/../../resources/views' => resource_path('views/vendor/livewire-bootstrap-modal')],
+            ['livewire-bootstrap-modal', 'livewire-bootstrap-modal:views']
+        );
+
+        Livewire::component('modals', Modals::class);
+    }
 }
